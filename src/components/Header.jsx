37be3f1c5ceb28770a95be1f0149311a394b4164
HaDock404 <delescluse.g@gmail.com>
2024-11-11 16:23:55 +0100

    header icon1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import '../styles/header.css'
 
+import Icon1 from '../components/Icon1'
+
 function Header() {
     return (
         <header id='header'>
-
+            <div id='part1'>
+                <div id='part1_el1'>
+                    <Icon1 />
+                </div>
+                <div id='part1_el2'></div>
+            </div>
         </header>
     )
 }
