88d3cf78fe0199a0d743c4e97af6f4e2d1240139
HaDock404 <delescluse.g@gmail.com>
2024-11-12 14:36:36 +0100

    intro_search placement

---
+++ src/components/IntroSearch.jsx
@@ -0,0 +1,23 @@
+import '../styles/intro_search.css'
+
+function IntroSearch() {
+    return (
+        <section id='intro_search'>
+            <article id='intro_search_part1'>
+                <div id='intro_search_part1A'>
+                    <div id='intro_search_part1A_el1'>Need icons for your website ? Choose from hundreds of free icons the ones that suit you.</div>
+                    <div id='intro_search_part1A_el2'>Easy to use, easy to customize, make your life easier.</div>
+                </div>
+                <div id='intro_search_part1B'>
+
+                </div>
+
+            </article>
+            <article id='intro_search_part2'>
+
+            </article>
+        </section>
+    )
+}
+
+export default IntroSearch
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header"
-import '../styles/homepage.css'
+import IntroSearch from "../components/IntroSearch"
+
 
 function Homepage() {
     return (
         <div id="main-container">
             <Header />
+            <IntroSearch />
         </div>
     )
 }
