7e1717972e1690e5321ff6d880cbc6974ce9a14b
HaDock404 <delescluse.g@gmail.com>
2024-11-13 11:15:31 +0100

    searchbar height

--- src/components/IntroSearch.jsx
+++ src/components/IntroSearch.jsx
@@ -10,6 +10,7 @@ function IntroSearch() {
                     <div id='intro_search_part1A_el1'>Need icons for your website ? Choose from hundreds of free icons the ones that suit you.</div>
                     <div id='intro_search_part1A_el2'>Easy to use, easy to customize, make your life easier.</div>
                 </div>
+                
                 <div id='intro_search_part1B'>
                     <div id='intro_search_part1B_el1'>
                         <div>Regular</div> 
@@ -19,13 +20,10 @@ function IntroSearch() {
                     <div id='intro_search_part1B_el3'>
                         <Icon3 />
                     </div>
-
                 </div>
-
             </article>
-            <article id='intro_search_part2'>
 
-            </article>
+            <article id='intro_search_part2'></article>
         </section>
     )
 }
