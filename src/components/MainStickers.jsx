c28810070022b3c18a52fbaf8c8f1c62fff41e75
HaDock404 <delescluse.g@gmail.com>
2025-06-15 10:02:34 +0200

    modale opened well

--- src/components/MainStickers.jsx
+++ src/components/MainStickers.jsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../styles/main_stickers.css'
 
+import Icon5 from './Icon5';
+import CopyButton from './CopyButton'
+
 function MainStickers({ items }) {
     const mainRef = useRef(null);
     const [isUp, setIsUp] = useState(false);
     const [isSmallMain, setIsSmallMain] = useState(window.innerWidth < 739);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedPlugin, setSelectedPlugin] = useState(null);
+    const [content, setContent] = useState("test ouverture");
+
+    const [selectedButton, setSelectedButton] = useState(1);
+    const [Copy, setCopy] = useState("")
+
+    const openModal = (sticker_name, sticker_path) => {
+      setSelectedPlugin(sticker_name);
+      setIsModalOpen(true);
+      setSelectedButton(1);
+    };
+
+
 
     useEffect(() => {
         const handleResize = () => {
@@ -59,7 +77,7 @@ function MainStickers({ items }) {
                   <div
                     className="box-main-stickers"
                     key={id}
-                    onClick={() => console.log("ok")}
+                    onClick={() => openModal(sticker_name, sticker_path)}
                   >
                     <div className="box-main-el1-stickers">
                       <img
@@ -87,6 +105,58 @@ function MainStickers({ items }) {
             )}
           </section>
         </section>
+
+
+
+
+        {/* Modale */}
+        {isModalOpen && (
+          <div className="modal-overlay" 
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+          >
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+              <div className='modal-placement'>
+                <div className='modal-placement-el1'>
+                  <div className='modal-placement-el1-box1'>
+                    <div className='modal-placement-el1-box1-icon'>
+                      
+                    </div>
+                    <div className='modal-placement-el1-box1-name'>
+                      
+                    </div>
+                  </div>
+                  <div className='modal-placement-el1-box2'>
+                    available in V1.0+ <br/>
+                    U+EB14
+                  </div>
+                </div>
+                <div className='modal-placement-el2'>
+                    <div className='modal-placement-el2-box1'>
+                      
+                      
+                      
+                      
+                    </div>
+                    <div className='modal-placement-el2-box2'>
+                      <div dangerouslySetInnerHTML={{ __html: content }} />
+                      <CopyButton text={Copy} />
+                    </div>
+                </div>
+                <button className='modal-button' 
+                  onClick={() => {
+                    setIsModalOpen(false);
+                    setContent("test ouverture")
+                  }}
+                >
+                  <Icon5 />
+                </button>
+              </div>
+
+            </div>
+          </div>
+        )}
       </>
     );
       
